9a01d03f2d8b295f788fadc91650b4bf0a7fe2b3
Adair Juneo <adair_juneo@hotmail.com>
2025-01-01 16:06:05 -0300

    create docker-compose file and prima-client configs.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import pluginJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -10,14 +11,19 @@ export default [
     files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: {
       'simple-import-sort': simpleImportSort,
-      'eslint-config-prettier': eslintConfigPrettier,
     },
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
+      'no-unused-vars': ['warn'],
+      semi: ['warn', 'always'],
+      quotes: ['error', 'single'],
+      'no-console': ['warn'],
     },
   },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettier,
   ...tseslint.configs.recommended,
 ];
---
+++ src/lib/db.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+
+const db = new PrismaClient({ log: ['warn', 'error'] });
+
+export default db;
